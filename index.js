3fea6e47ce10b07fc798c2efe6cbfa8ac27c9a38
garrettdick <garrettd0511@gmail.com>
2021-09-22 16:20:12 -0500

    commit tree 64b033c0e027a0f7192c7e6e4274703f6e8da266

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 // Code your solution in this file!
+//create const variable returnFirstTwoDrivers
+//set to annonymos function
+//accepts argument of array of drivers
+//return first 2 drivers
 
 const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
 
-const returnFirstTwoDrivers = () => drivers.slice(0, 2)
+// const returnFirstTwoDrivers = () => drivers.slice(0, 2)
 
-const  returnLastTwoDrivers = () => drivers.slice(2, 4)
+const returnFirstTwoDrivers = function(arrayOfDrivers){
+    return arrayOfDrivers.slice(0,2)
+}
+
+// const  returnLastTwoDrivers = () => drivers.slice(2, 4)
+
+const returnLastTwoDrivers = function(arrayOfDrivers){
+    return arrayOfDrivers.slice(-2)
+}
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
-const createFareMultiplier = i => {
+// const createFareMultiplier = i => {
+//     return function(fare) {
+//         return fare * i;
+//     }
+// }
+
+// function called createFareMultiplier
+// takes in an argument of an interger(some kind of number)
+// It should return a function that takes in an argument of fare
+// multiply fare by interger that createFareMultiplier took
+
+function createFareMultiplier(multiplier) {
     return function(fare) {
-        return fare * i;
+        return fare * multiplier
     }
 }
 
-// const fareDoubler = () => {}
+
+// const fareDoubler = (theFare) => createFareMultiplier(2)(theFare);
 const fareDoubler = createFareMultiplier(2)
 
-// const fareTripler = () => {}
+// const fareTripler = (theFare) => createFareMultiplier(3)(theFare);
 const fareTripler = createFareMultiplier(3)
+
 
 function selectDifferentDrivers(arrayOfDrivers,returnFirstTwoDrivers) {
     return returnFirstTwoDrivers(drivers)
 };
+
